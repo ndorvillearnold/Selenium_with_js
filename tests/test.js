a7be30453703d48a829f5544294861c2dff11a96
Naomy <ndorvillearnold@gmail.com>
2024-04-13 19:17:33 -0400

    added  code for login

--- tests/test.js
+++ tests/test.js
@@ -15,22 +15,29 @@ async function test_case() {
     await driver.findElement(By.partialLinkText("Sign in")).click();
 
     //display title so we can make sure its correct 
-    console.log(await driver.getTitle())
+    console.log(await driver.getTitle());
 
     //end test if title does not match 
-    if (await driver.getTitle() === "Sign in to GitHub . GitHub") {
+    if (await driver.getTitle() === "Sign in to GitHub · GitHub") {
 
         console.log("test number 1 is a success")
 
     } else {
 
-        console.log("Test one failed, return ")
-        return
-    };
+        console.log("Test one failed, return");
+        return;
+    }
 
 
-    //enter name int the fields... I inspected the page to get the attribute
-    await driver.findElement(By.name(login)).sendKeys("")
+    //enter username and password int the fields... I inspected the page to get the attribute
+    await driver.findElement(By.name("login")).sendKeys("naomyATestAccount", Key.RETURN);
+
+    await driver.findElement(By.id("password")).sendKeys("N@0myT3$tacc!", Key.RETURN);
+
+    await driver.findElement(By.href("password")).sendKeys("/naomyATestAccount/Selenium_with_js_testAcc").click();
+
+
+
 
     // Wait for 10 seconds and then quit the driver
     //     setTimeout(async function () {
