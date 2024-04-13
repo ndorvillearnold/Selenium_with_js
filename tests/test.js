deba7f3dd85cedd1369ca4c47599cb1dc3e701fb
Naomy <ndorvillearnold@gmail.com>
2024-04-13 17:15:03 -0400

    first commit

---
+++ tests/test.js
@@ -0,0 +1,23 @@
+const { Builder, By, Key } = require("selenium-webdriver");
+require("geckodriver");
+
+async function test_case() {
+
+    //creates the driver
+    let driver = await new Builder().forBrowser("firefox").build();
+
+    // send driver to the website then Wait for Google homepage to appear
+    await driver.get("https://github.com");
+
+    // (grab an element from the page) Type "Hello World" into the search input field and press Enter
+    await driver.findElement(By.xpath("q")).sendKeys("Hello World", Key.RETURN);
+
+    // Wait for 10 seconds and then quit the driver
+    //     setTimeout(async function () {
+    //         await driver.quit();
+    //     }, 10000);
+    // }
+
+}
+//calling the function above
+test_case();
