054a6b9c59402eac703dcd58c3d75f7f51106105
Naomy <ndorvillearnold@gmail.com>
2024-04-13 19:22:06 -0400

    added to read me file

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,8 @@
 const { Builder, By, Key } = require("selenium-webdriver");
 require("geckodriver");
 
+
+//run code with npm test
 async function test_case() {
 
     //creates the driver
@@ -11,6 +13,31 @@ async function test_case() {
 
     // (grab an element from the page) Type "Hello World" into the search input field and press Enter
     await driver.findElement(By.partialLinkText("Sign in")).click();
+
+    //display title so we can make sure its correct 
+    console.log(await driver.getTitle());
+
+    //end test if title does not match 
+    if (await driver.getTitle() === "Sign in to GitHub · GitHub") {
+
+        console.log("test number 1 is a success")
+
+    } else {
+
+        console.log("Test one failed, return");
+        return;
+    }
+
+
+    //enter username and password int the fields... I inspected the page to get the attribute
+    await driver.findElement(By.name("login")).sendKeys("naomyATestAccount", Key.RETURN);
+
+    await driver.findElement(By.id("password")).sendKeys("N@0myT3$tacc!", Key.RETURN);
+
+    await driver.findElement(By.href("password")).sendKeys("/naomyATestAccount/Selenium_with_js_testAcc").click();
+
+
+
 
     // Wait for 10 seconds and then quit the driver
     //     setTimeout(async function () {
